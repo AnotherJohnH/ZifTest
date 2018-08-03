b25e7ed5d2535b2724247e51905edafc25b9193e
John D. Haughton <anotherjohnh@gmail.com>
2018-08-03 22:14:19 +0100

    Add some options

--- Source/scrape.cpp
+++ Source/scrape.cpp
@@ -21,23 +21,29 @@
 //------------------------------------------------------------------------------
 
 #include "STB/ConsoleApp.h"
+#include "STB/Option.h"
 #include "STB/XML.h"
 #include "STB/Http.h"
 
 
-#define  PROGRAM         "ZifTest"
-#define  DESCRIPTION     "Test suite for Zif"
+#define  PROGRAM         "scrape"
+#define  DESCRIPTION     "Scrape IF archive using http for test files (please be nice!)"
 #define  LINK            "https://github.com/AnotherJohnH/"
 #define  COPYRIGHT_YEAR  "2018"
 #define  AUTHOR          "John D. Haughton"
 #define  VERSION         PROJ_VERSION
 
 
-class ZifTestApp : public STB::ConsoleApp
+class ScrapeApp : public STB::ConsoleApp
 {
 private:
+   STB::Option<const char*> host{   'H', "host",  "Remote hostname",  "ifarchive.org"};
+   STB::Option<const char*> path{   'P', "path",  "Remote path",      "/if-archive/games/zcode/"};
+   STB::Option<const char*> ext{    'E', "ext",   "File extension",   "z5"};
+   STB::Option<const char*> cache{  'C', "cache", "Cache directory",  "cache"};
+   STB::Option<const char*> list_fn{'L', "list",  "List output file", "list.json"};
+
    void findLinks(STB::Http&               http,
-                  const std::string&       path,
                   const STB::XML::Element& xml,
                   PLT::File&               list,
                   bool&                    first)
@@ -50,41 +56,40 @@ private:
 
             // TODO handle hrefs that include a path
 
-            for(unsigned i=1; i<=8; i++)
+            std::string dot_ext = ".";
+            dot_ext += ext;
+
+            if (href.find(dot_ext) != std::string::npos)
             {
-               std::string ext = ".z";
-               ext += '0' + i;
+               std::string local_path = (const char*)path;
+               local_path += href;
 
-               if (href.find(ext) != std::string::npos)
+               std::string file = (const char*) cache;
+               file += '/';
+               file += href;
+
+               if (PLT::File::size(file.c_str()) == 0)
                {
-                  std::string local_path = path;
-                  local_path += href;
+                  printf("GET http://%s%s as \"%s\"",
+                         (const char*)host, local_path.c_str(), file.c_str());
+                  bool ok = http.getFile(local_path, file);
+                  printf(" - %s\n", ok ? "OK" : "FAIL");
+              }
 
-                  std::string file = "cache/";
-                  file += href;
+              if (first)
+              {
+                 first = false;
+              }
+              else
+              {
+                 list.printf(",\n");
+              }
 
-                  if (PLT::File::size(file.c_str()) == 0)
-                  {
-                     printf("get '%s'", href.c_str());
-                     bool ok = http.getFile(local_path, file);
-                     printf(" - %s\n", ok ? "OK" : "FAIL");
-                  }
-
-                  if (first)
-                  {
-                     first = false;
-                  }
-                  else
-                  {
-                     list.printf(",\n");
-                  }
-
-                  list.printf("   [\"%s\"]", file.c_str());
-               }
+               list.printf("   [\"%s\"]", file.c_str());
             }
          }
 
-         findLinks(http, path, element, list, first);
+         findLinks(http, element, list, first);
       }
    }
 
@@ -92,19 +97,23 @@ private:
    {
       STB::Http http;
 
-      if(http.open("ifarchive.org"))
+      if(http.open((const char*)host))
       {
-         if(http.getFile("/if-archive/games/zcode/", "cache/index.html"))
+         std::string file = (const char*) cache;
+         file += '/';
+         file += "index.html";
+
+         if(http.getFile((const char*)path, file))
          {
-            STB::XML::Document xml("cache/index.html", /* require_prolog */ false);
-            PLT::File          list(nullptr, "list.json");
+            STB::XML::Document xml(file, /* require_prolog */ false);
+            PLT::File          list(nullptr, list_fn);
 
             if (list.openForWrite())
             {
                list.printf("[\n");
 
                bool first = true;
-               findLinks(http, "/if-archive/games/zcode/", xml, list, first);
+               findLinks(http, xml, list, first);
 
                list.printf("\n]\n");
             }
@@ -117,7 +126,7 @@ private:
    }
 
 public:
-   ZifTestApp(int argc, const char* argv[])
+   ScrapeApp(int argc, const char* argv[])
       : ConsoleApp(PROGRAM, DESCRIPTION, LINK, AUTHOR, VERSION, COPYRIGHT_YEAR)
    {
       parseArgsAndStart(argc, argv);
@@ -127,5 +136,5 @@ public:
 
 int main(int argc, const char* argv[])
 {
-   ZifTestApp(argc, argv);
+   ScrapeApp(argc, argv);
 }
